899a20bbec0e7551a8a54bf8a65e76d3f96cd904
theankushrai <iamankushrai@gmail.com>
2022-04-01 14:58:20 +0530

    1. image popup on upload implemented

--- public/js/model2.js
+++ public/js/model2.js
@@ -3,6 +3,7 @@ const image_input = document.querySelector("#image_input");
 var uploaded_image;
 
 image_input.addEventListener('change', function() {
+  document.getElementById("display_image").style.setProperty("visibility","visible");
   const reader = new FileReader();
   reader.addEventListener('load', () => {
     uploaded_image = reader.result;
