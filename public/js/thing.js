a3b70b99eb75175a011ac2fcf8473c508f5287af
theankushrai <iamankushrai@gmail.com>
2022-04-07 13:22:42 +0530

    1.local database completed

--- public/js/thing.js
+++ public/js/thing.js
@@ -1,10 +1,19 @@
 window.onload=function(){
-    replacetext();
+    replacething();
 }
 
-function replacetext(){
+let text='{"person":{"image":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/1200px-Person_icon_BLACK-01.svg.png","desciption":"Humans cannot be recycled","videourl":"https://www.youtube.com/embed/inpok4MKVLM"}}';
+const obj = JSON.parse(text);
+
+function replacething(){
+
+    console.log(obj.person.description);
     document.getElementById('thingname').textContent=sessionStorage.getItem('thing');
-    console.log('thing name replaced');
+    document.getElementById('thingdesciption').innerHTML= obj.person.desciption;
+    document.getElementById('thingimage').setAttribute('src',"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/1200px-Person_icon_BLACK-01.svg.png");
+    document.getElementById('thingvideo').setAttribute('src',obj.person.videourl);
+    console.log('thing replaced');
     sessionStorage.removeItem('thing');
     console.log('thing name deleted');
+    
 }
